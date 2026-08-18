92107382c0bd83ebe7ce5e6198b0d7b904ade3e0
juanskieee <juancarlosdelacruz5334@gmail.com>
2026-08-18 21:24:35 +0800

    Fix manifest 403 ordering, same-origin API_URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@
 // Update MAPBOX_TOKEN with your Mapbox public token
 // VAPID_PUBLIC_KEY comes from your web-push keygen step
 
-export const API_URL         = 'https://api.glorikar.com'; // TODO: set on deploy
+export const API_URL         = ''; // same-origin: frontend and API are served from one host
 export const MAPBOX_TOKEN    = 'pk.YOUR_MAPBOX_TOKEN';     // TODO: set on deploy
 export const VAPID_PUBLIC_KEY = 'BMVrElpHJyBOqIkXog9bGWbuqSkp84NpUALeSCp_UNCodhQ88Ysn00btPMZGGBjQRheVoStaAv0brATJZ0TPY2c';
